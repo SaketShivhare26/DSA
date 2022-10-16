0b5a255e83259abdab1ecff07a0c2fb4855a25d8
Saket shivhare <71497770+SaketShivhare26@users.noreply.github.com>
2022-10-16 09:27:29 +0530

    Update WaveForm2dArray.cpp

--- WaveForm2dArray.cpp
+++ WaveForm2dArray.cpp
@@ -1,3 +1,37 @@
+/*
+The basic idea of this approach is to print the elements row-wise. The elements are printed in two ways; top-down and bottom-up. It has been observed that the odd rows are printed in a bottom-up manner, and even ones are printed in a top-down manner. We will use two loops, the first loop will iterate through the number of columns, and the second loop will print the element row-wise.
+
+ 
+
+Here is the algorithm:
+
+ 
+
+Run a loop from ‘I’ = 0 to ‘M_COL’ (Number of columns given)
+If the current row is even, then the elements will be printed from top to bottom.
+Run an inner loop from ‘J’ = 0 to ‘N_ROW’ (Number of rows given)
+Print all the elements.
+Else if the current row is odd, then the elements will be printed from bottom to top.
+Run another loop from ‘J’ = ‘N_ROWS - 1’ to 0.
+Print all the elements.
+Time Complexity
+O(N * M), where N is the number of rows and M is the number of columns.
+
+ 
+
+Because we are iterating to every element of the given matrix, as N * M elements present in the matrix, the time complexity is O(N * M).
+
+Space Complexity
+O(M*N), where N is the number of rows and M is the number of columns.
+
+ 
+
+Because we are storing all the elements in a vector, as the total number of elements in the given matrix is M*N, hence the space complexity is O(M*N).
+
+
+*/
+
+
 #include <bits/stdc++.h> 
 vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
 {
